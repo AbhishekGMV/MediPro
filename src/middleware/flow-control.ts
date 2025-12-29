804bfe33c6772cc7db445d5802949673e20e4b38
abhishekgmv <abhishek.mvg@gmail.com>
2025-12-29 17:19:59 +0530

    OTEL instrumentation setup

--- src/middleware/flow-control.ts
+++ src/middleware/flow-control.ts
@@ -1,18 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import { INTERACTION_ID } from "../utils/constants";
+import { getInteractionId, interactionStorage } from "../utils/logger";
 
 const { v4: uuidv4, validate: uuidValidate } = require("uuid");
 
 export default function assignInteractionId(
   req: Request,
   res: Response,
-  next: NextFunction,
+  next: NextFunction
 ): void {
-  let interactionId = req.headers[INTERACTION_ID];
-  if (!interactionId) {
-    interactionId = uuidv4();
-    req.headers[INTERACTION_ID] = interactionId;
-  }
-  res.set({ [INTERACTION_ID]: interactionId });
-  next();
+  let interactionId = req.headers[INTERACTION_ID] || uuidv4();
+  interactionStorage.run({ interactionId: interactionId.toString() }, () => {
+    req.headers[INTERACTION_ID] = interactionId.toString();
+    next();
+  });
 }
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,8 +1,30 @@
 import { createLogger, format, transports } from "winston";
+import { AsyncLocalStorage } from "async_hooks";
+
+export const interactionStorage = new AsyncLocalStorage<{
+  interactionId: string;
+}>();
+
+export const getInteractionId = () => {
+  return interactionStorage.getStore()?.interactionId;
+};
 
 const logger = createLogger({
   level: "info",
-  format: format.combine(format.timestamp(), format.json()),
+  format: format.combine(
+    format.timestamp(),
+    format.json(),
+    format.printf(({ timestamp, level, message, ...meta }) => {
+      const interactionId = getInteractionId();
+      return JSON.stringify({
+        timestamp,
+        level,
+        message,
+        interactionId,
+        ...meta,
+      });
+    })
+  ),
   transports: [
     new transports.Console(),
     new transports.File({ filename: "app.log" }),
